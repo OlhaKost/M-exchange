bf88a81462bed6865e7b75fa9a0a403160ca0a00
Olha Kostiuchenko <pugach220@gmail.com>
2022-06-16 18:27:45 +0200

    Firs commit

---
+++ exchange.js
@@ -0,0 +1,15 @@
+let inputUSD1 = document.getElementById('USD1');
+let inputUAH1 = document.getElementById('UAH1');
+let inputPLN1 = document.getElementById('PLN1');
+let inputEUR1 = document.getElementById('EUR1');
+let inputUSD2 = document.getElementById('USD2');
+let inputUAH2 = document.getElementById('UAH2');
+let inputPLN3 = document.getElementById('PLN2');
+let inputEUR4 = document.getElementById('EUR2');
+
+
+switch () {
+
+case "a"
+
+}
